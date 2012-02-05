5da2129edc421ad0d9d765b485c1784b52334387
Kevin Altman <kevin@thinkrelevance.com>
2012-02-05 15:56:52 -0500

    adding latest single Dribbble

---
+++ source/_posts/2012-02-05-baseline-grid-bookmarklet.markdown
@@ -0,0 +1,9 @@
+---
+layout: post
+title: "baseline grid bookmarklet"
+date: 2012-02-05 14:56
+comments: true
+categories: 
+---
+
+<a href="javascript:%20(function%20()%20%7B%20%20%20%20%20%20%20%20var%20jsCode%20=%20document.createElement('script');%20%20%20%20%20%20%20%20jsCode.setAttribute('src',%20'http://dl.dropbox.com/u/37716909/baseline-grid/griditnow.js');%20%20%20%20%20%20%20%20%20%20document.body.appendChild(jsCode);%20%20%20%20%20%20%20%20%20%20%20%7D());">Bookmark me!</a>
---
+++ source/javascripts/dribbble.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+  $.jribbble.getShotsByPlayerId('itsthatguy', function (playerShots) {
+    console.log(playerShots.shots[0].title)
+    console.log(playerShots.shots[0].player.name)
+    console.log(playerShots.shots[0].image_teaser_url)
+    
+    var html = [];
+    shot = playerShots.shots[0];
+    //html.push('<li><h3>' + shot.title + '</h3>');
+    //html.push('<h4>by ' + shot.player.name + '</h4><a href="' + shot.url + '">');
+    html.push('<a href="' + shot.url + '"><h4>Dribbble</h4>')
+    html.push('<img src="' + shot.image_teaser_url + '" ');
+    html.push('alt="' + shot.title + '"></a>');
+
+    $('#dribbble').html(html.join(''));
+  }, {page: 1, per_page: 10})
+});
