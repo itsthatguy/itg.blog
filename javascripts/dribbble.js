21558eec4633703bdf857d220448837d69ff7d10
Kevin Altman <kevin@thinkrelevance.com>
2012-02-05 15:40:51 -0500

    Site updated at 2012-02-05 20:40:51 UTC

--- javascripts/dribbble.js
+++ javascripts/dribbble.js
@@ -5,12 +5,12 @@ $(document).ready(function () {
     console.log(playerShots.shots[0].image_teaser_url)
     
     var html = [];
-    $.each(playerShots.shots, function (i, shot) {
-      html.push('<li><h3>' + shot.title + '</h3>');
-      html.push('<h4>by ' + shot.player.name + '</h4><a href="' + shot.url + '">');
-      html.push('<img src="' + shot.image_teaser_url + '" ');
-      html.push('alt="' + shot.title + '"></a></li>');
-    });
+    shot = playerShots.shots[0];
+    //html.push('<li><h3>' + shot.title + '</h3>');
+    //html.push('<h4>by ' + shot.player.name + '</h4><a href="' + shot.url + '">');
+    html.push('<a href="' + shot.url + '"><h2>Dribbble</h2>')
+    html.push('<img src="' + shot.image_teaser_url + '" ');
+    html.push('alt="' + shot.title + '"></a>');
 
     $('#dribbble').html(html.join(''));
   }, {page: 1, per_page: 10})
